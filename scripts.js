e049e4a6bb2d3ce3045dca1056cc0752c305726d
Anna Smolentzov <annasmolentzov@gmail.com>
2019-03-06 16:31:55 -0700

    Refactors event listeners to use functions

--- scripts.js
+++ scripts.js
@@ -7,10 +7,7 @@ $(document).ready(function() {
   
   $('.set-range').click(function(event) {
     event.preventDefault();
-    rangeMinimum = parseInt($('#minimum').val());
-    rangeMaximum = parseInt($('#maximum').val());
-    setGameNumber(rangeMinimum, rangeMaximum);
-    $('.range-display').text(`Current Range: ${rangeMinimum} to ${rangeMaximum}`)
+    setRange();
   });
   
   $('.submit-guess').click(function(event) {
@@ -24,28 +21,12 @@ $(document).ready(function() {
   });
   
   $('.reset-button').on('click', function() {
-    $('.guess-field').val('');
-    $('.guess-field').prop('disabled', false);
-    $('#minimum').val('');
-    $('#maximum').val('');
-    rangeMinimum = 0;
-    rangeMaximum = 10;
-    $('.range-display').text(`Current Range: ${rangeMinimum} to ${rangeMaximum}`);
-    $('.guess-container').hide();
-    $('.range-container').show();
-    setGameNumber(rangeMinimum, rangeMaximum);
+    resetGame();
   });
   
   $('.next-round-button').on('click', function(event) {
     event.preventDefault();
-    $('.guess-container').hide();
-    $('.next-round-button').hide();
-    $('.guess-field').prop('disabled', false);
-    $('.guess-field').val('');
-    rangeMinimum -= 10;
-    rangeMaximum += 10;
-    $('.range-display').text(`Current Range: ${rangeMinimum} to ${rangeMaximum}`);
-    setGameNumber(rangeMinimum, rangeMaximum);
+    getNextRound();
   })
 });
 
@@ -78,5 +59,35 @@ function makeGuess(min, max, correct) {
 
 function setGameNumber(min, max) {
   gameNumber = Math.floor((Math.random() * (max - min + 1)) + min);
-  console.log(gameNumber);
+};
+
+function setRange() {
+  rangeMinimum = parseInt($('#minimum').val());
+  rangeMaximum = parseInt($('#maximum').val());
+  setGameNumber(rangeMinimum, rangeMaximum);
+  $('.range-display').text(`Current Range: ${rangeMinimum} to ${rangeMaximum}`)
+};
+
+function resetGame() {
+  $('.guess-field').val('');
+  $('.guess-field').prop('disabled', false);
+  $('#minimum').val('');
+  $('#maximum').val('');
+  rangeMinimum = 0;
+  rangeMaximum = 10;
+  $('.range-display').text(`Current Range: ${rangeMinimum} to ${rangeMaximum}`);
+  $('.guess-container').hide();
+  $('.range-container').show();
+  setGameNumber(rangeMinimum, rangeMaximum);
+};
+
+function getNextRound() {
+  $('.guess-container').hide();
+  $('.next-round-button').hide();
+  $('.guess-field').prop('disabled', false);
+  $('.guess-field').val('');
+  rangeMinimum -= 10;
+  rangeMaximum += 10;
+  $('.range-display').text(`Current Range: ${rangeMinimum} to ${rangeMaximum}`);
+  setGameNumber(rangeMinimum, rangeMaximum);
 };
